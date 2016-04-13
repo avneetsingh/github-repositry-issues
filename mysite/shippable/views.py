48cfb69c0cf5f654ea979eb72ec29e9a1b1e00da
Avneet Singh <avneetsingh114@gmail.com>
2016-04-13 13:03:55 +0530

    view

--- mysite/shippable/views.py
+++ mysite/shippable/views.py
@@ -26,7 +26,7 @@ class Struct:
             for n in range(len(argv)):
                 setattr(self, attrs[n], argv[n])
 
-
+#All Issues Fields used to display the Table Columns
 class Post(Struct):
     title=""
     post_id=""
@@ -41,23 +41,23 @@ def index(request):
     return render(request,'shippable/home.html')
 
 def process(request):
-        first=[]
-        second=[]
-        third=[]
-        fourth=[]
+        first=[]  #to store indexes of issues for  OpenIssues noOfOpenIssuesInLast24hr , noOfOpenIssuesMoreThan24hLessThan7d , noOfOpenIssuesMoreThan7d
+        second=[]  #to store indexes of issues for OpenIssuesInLast24hr
+        third=[]   #to store indexes of issues for OpenIssuesMoreThan24hLessThan7d
+        fourth=[]  #to store indexes of issues for OpenIssuesMoreThan7d
         noOfOpenIssues=0
         noOfOpenIssuesInLast24hr = 0
         noOfOpenIssuesMoreThan24hLessThan7d = 0
         noOfOpenIssuesMoreThan7d = 0
         instancelist = []
-        search_id=request.POST.get('textfield',None)
-        repositoryName=request.POST.get('textfield2',None)
-        accesstoken=request.POST.get('textfield3',None)
+        search_id=request.POST.get('textfield',None) #fetch USERNAME
+        repositoryName=request.POST.get('textfield2',None) #fetch RepositoryName
+        accesstoken=request.POST.get('textfield3',None)  #Optional Accesstoken of USer because for nontoken user can have only limited number of acccess to the API 
         #""
-        url="https://api.github.com/repos/"+search_id+"/"+repositoryName+"?access_token="+str(accesstoken)
+        url="https://api.github.com/repos/"+search_id+"/"+repositoryName
         #https://api.github.com/repos/shippable/support
         response = requests.get(url)
-        try:
+        try: #if Client gives Valid repository name and his User Name 
             data2 = json.loads(response.content)
             open_issue = data2['open_issues']
             j=0
@@ -66,7 +66,7 @@ def process(request):
             noOfOpenIssues=open_issue
             
             for i in range(5):
-                url= "https://api.github.com/repos/"+search_id+"/"+repositoryName+"/issues?page="+str(i+1)+"&per_page=100&access_token=cdd25761e5458bf25f257f4164f40a7a5e8d7422"
+                url= "https://api.github.com/repos/"+search_id+"/"+repositoryName+"/issues?page="+str(i+1)+"&per_page=100"
                 response = requests.get(url)
                 data = json.loads(response.content)
                 #print data
@@ -86,45 +86,45 @@ def process(request):
                     instancelist.append(temp)
                     a= dt.datetime((int)(time1[0]),(int)(time1[1]),(int)(time1[2]),(int)(time1[3]),(int)(time1[4]),(int)(time1[5]))
                     b= dt.datetime((int)(time2[0]),(int)(time2[1]),(int)(time2[2]),(int)(time2[3]),(int)(time2[4]),(int)((float)(time2[5])))
-                    dif = ((b-a).total_seconds()/(60*60*24))
+                    dif = ((b-a).total_seconds()/(60*60*24))# calculate difference between current time stamp and atupdated issue timestamp in days
                     #print j ,time1, time2 ,dif
-                    if(dif<=1.00):
+                    if(dif<=1.00): #if updation  timestamp is within 24 hrs
                         noOfOpenIssuesInLast24hr=noOfOpenIssuesInLast24hr+1
                         second.append(len(instancelist)-1)
                         first.append(len(instancelist)-1)
-                    if(dif>1.0 and dif <=7.0):
+                    if(dif>1.0 and dif <=7.0): #if updation timestamp is within more than 1 and less than 7 days
                         noOfOpenIssuesMoreThan24hLessThan7d= noOfOpenIssuesMoreThan24hLessThan7d+1
                         third.append(len(instancelist)-1)
                         first.append(len(instancelist)-1)
-                    if(dif>7.0):
+                    if(dif>7.0):#if updation  timestamp is more than 7 days
                         noOfOpenIssuesMoreThan7d = noOfOpenIssuesMoreThan7d+1
                         fourth.append(len(instancelist)-1)
                         first.append(len(instancelist)-1)
             #print noOfOpenIssues, noOfOpenIssuesInLast24hr , noOfOpenIssuesMoreThan24hLessThan7d , noOfOpenIssuesMoreThan7d
-            firsttitle =[]
-            firstUser=[]
-            firstcreatedat=[]
-            firstupdatedat=[]
-            firstcomments=[]
-            firsturl=[]
-            secondtitle =[]
-            secondUser=[]
-            secondcreatedat=[]
-            secondupdatedat=[]
-            secondcomments=[]
-            secondurl=[]
-            thirdtitle =[]
-            thirdUser=[]
-            thirdcreatedat=[]
-            thirdupdatedat=[]
-            thirdcomments=[]
-            thirdurl=[]
-            fourthtitle =[]
-            fourthUser=[]
-            fourthcreatedat=[]
-            fourthupdatedat=[]
-            fourthcomments=[]
-            fourthurl=[]
+            firsttitle =[] #openIssues titles
+            firstUser=[]   #openIssues Usernames
+            firstcreatedat=[] #openIssues created date
+            firstupdatedat=[]  #openIssues updated date
+            firstcomments=[]   #no of comments in each OpenIssues
+            firsturl=[]    #urls of OpenIssuesInLast24hr
+            secondtitle =[]   #OpenIssuesInLast24hr titles
+            secondUser=[]     #OpenIssuesInLast24hr Usernames
+            secondcreatedat=[]  #OpenIssuesInLast24hr created date
+            secondupdatedat=[]  #OpenIssuesInLast24hr updated date
+            secondcomments=[]   #no of comments in each OpenIssuesInLast24hr
+            secondurl=[]    #urls of OpenIssuesInLast24hr
+            thirdtitle =[]   #OpenIssuesMoreThan24hLessThan7d titles
+            thirdUser=[]    #openIssOpenIssuesMoreThan24hLessThan7d Usernames
+            thirdcreatedat=[]  #OpenIssuesMoreThan24hLessThan7d created date
+            thirdupdatedat=[]   #OpenIssuesMoreThan24hLessThan7d updated date
+            thirdcomments=[]    #no of comments in each OpenIssuesMoreThan24hLessThan7d
+            thirdurl=[]         #urls of OpenIssuesMoreThan24hLessThan7d
+            fourthtitle =[]  #OpenIssuesMoreThan7d titles
+            fourthUser=[]    #OpenIssuesMoreThan7d Usernames
+            fourthcreatedat=[]  #OpenIssuesMoreThan7d created date
+            fourthupdatedat=[]   #OpenIssuesMoreThan7d updated date
+            fourthcomments=[]    #no of comments in each OpenIssuesMoreThan7d
+            fourthurl=[]        #urls of OpenIssuesMoreThan7d
             for each in second:
                 secondtitle.append(instancelist[each].title)
                 secondUser.append(str(instancelist[each].user))
